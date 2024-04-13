9a2d4f1906ea6442f5b1c9f0ffe8b897c798746c
João Lucas B. Lauritzen <lukemagike@gmail.com>
2024-04-13 00:15:18 -0300

    Mais pro hpp

--- StarWarsFangame/scene.hpp
+++ StarWarsFangame/scene.hpp
@@ -5,17 +5,30 @@ class Scene {
     
     public:
         Scene();
-        //~Scene();
+        ~Scene();
         //void TextureDraw();
         //void DrawText();
         //void DrawBackground();
         //void ScrollText();
         //void Effects();
         void OpeningBegin();
+        void TextBegin();
+        void TextToTexture();
     private:
         Texture2D logo;
         Texture2D background;
         Vector2 position;
         float alpha;
+        float currentTime;
+        float screenWidth;
+        float screenHeight;
+        Font fontf;
+        Vector2 scroll;
+        Music openingm;
+        Camera3D camera;
+        Camera2D cameraVector2;
+        int camera_mode;
+        Shader textshader;
+        RenderTexture2D target;
 
 };

56e98567b38dd203c82601fc71fca5744b70c0f2
João Lucas B. Lauritzen <lukemagike@gmail.com>
2024-04-20 22:22:04 -0300

    Implementação inicial frontend

---
+++ StarWarsFangame/frontend.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <raylib.h>
+#include "scene.hpp"
+
+class Frontend {
+
+    public:
+        Frontend();
+        ~Frontend();
+        void MainFrontend();
+
+
+    private:
+
+};

f8d9c0c039daf4c985127ebf11df1a7fb3ae00e1
João Lucas B. Lauritzen <lukemagike@gmail.com>
2024-04-25 16:57:09 -0300

    Mais

--- StarWarsFangame/frontend.cpp
+++ StarWarsFangame/frontend.cpp
@@ -8,12 +8,12 @@
 
 Frontend::Frontend() {
 
-    CurrentTime = GetTime();
 
-         OpeningScene = true;
+
+         OpeningScene = false;
          Mode2D = true;
          Mode3D = false;
-         MenuMode = false;
+         MenuMode = true;
 
         offset = 150.f;
 
@@ -25,12 +25,24 @@ Frontend::Frontend() {
 //    ModeSet = true;
 
     background = LoadTexture("graphics/backgroundmenu.png");
+    logo = LoadTexture("graphics/logotransparent2size.png");
     
 //    this->background = background;
 
     MainMenu = false;
     HasPressedTitle = false;
-    alpha = 1.f;
+    alphaTitle = 1.f;
+    alphaMenu = 0.f;
+
+    IsSettings = false;
+
+    settings.VideoSettings = false;
+
+    StartGame = false;
+
+    CurrentTime = GetTime();
+
+    Time = 0.0f;
 
 }
 
@@ -40,21 +52,22 @@ Frontend::~Frontend() {
 
 void Frontend::MainFrontend() {
 
+    int centerOffset = 70;
+    int verticalOffset = 40;
 
+    Rectangle logoRectangle = (Rectangle){0, 0, logo.width, logo.height};
+    Vector2 logoCenter = {logo.width/2, logo.height/2};
+    Vector2 position {screenWidth /2, screenHeight /3};
 
     DrawTexture(background, 0, 0, WHITE);
+//    DrawTextureEx(logo, (Vector2){screenWidth/2 - centerOffset, 50.f}, 0.f, 0.5f, WHITE);
+    DrawTexturePro(logo, logoRectangle, (Rectangle){float(position.x), float(position.y), float(logoRectangle.width), float(logoRectangle.height)}, logoCenter, 0, WHITE);
 
     GuiSetStyle(BUTTON, TEXT_ALIGNMENT, 50);                
 
-    if (GuiButton((Rectangle){25, 255, 125, 30}, GuiIconText(ICON_BOX, "TESTBUTTON")));
+//    if (GuiButton((Rectangle){25, 255, 125, 30}, GuiIconText(ICON_BOX, "TESTBUTTON")));
 
-    DrawFPS(10, 10);
-
-    if(!MainMenu) // title
-    {
-    DrawTextEx(GetFontDefault(), Title2, title, 10.f, 5.f, Fade(WHITE, alpha));
-
-    }
+//    DrawFPS(10, 10);
 
     if(IsKeyPressed(KEY_ENTER))
     {
@@ -63,56 +76,123 @@ void Frontend::MainFrontend() {
 
     if(HasPressedTitle)
     {
-        alpha -= 0.10f; //GetTime() / 50;
+        alphaTitle -= 0.10f; //GetTime() / 50;
         
-        if(alpha == 0.0f)
+        if(alphaTitle < 0.0f)
         {  
             MainMenu = true;
+            alphaMenu += 0.10f;
         }
+    }
 
+//  DrawRectanglePro(rec, (Vector2){, 0}, 0, Fade(WHITE, 0.5f));
+//  Rectangle rec = {screenWidth/2, screenHeight/2, 100, 40};
 
+    switch(MainMenu) 
+    {
+        case true:
+            if (GuiButton((Rectangle){960 - centerOffset, 600, 125, 30}, "Novo Jogo")) OpeningScene = true, Mode3D = true, Mode2D = false; //x, y, width, height
+            if (GuiButton((Rectangle){960 - centerOffset, 650, 125, 30}, "Configurações")) IsSettings = !IsSettings;
+            if (GuiButton((Rectangle){960 - centerOffset, 700, 125, 30}, "Fechar Jogo")) CloseWindow();
+            
+            if (IsSettings)
+            {
+                DrawRectangle(GetScreenWidth() /2 - 300, GetScreenHeight() /2 - 40, 200, 300, Fade(WHITE, 0.5f));
+
+                if (GuiTextBox((Rectangle){800 - centerOffset, 500, 125, 30}, type.Config0, 64, textBoxEditMode)) settings.VideoSettings = !settings.VideoSettings;
+                if (GuiTextBox((Rectangle){800 - centerOffset, 500 + 40, 125, 30}, type.Config1, 64, textBoxEditMode)) settings.AudioSettings = !settings.AudioSettings;
+                if (GuiTextBox((Rectangle){800 - centerOffset, 500 + 80, 125, 30}, type.Config2, 64, textBoxEditMode)) settings.InputSettings = !settings.InputSettings;
+                if (GuiTextBox((Rectangle){800 - centerOffset, 500 + 120, 125, 30}, type.Config3, 64, textBoxEditMode)) settings.AccessSettings = !settings.AccessSettings;
+
+                // método settings
+
+                Settings();
+            }
+
+        break;
+        case false:
+            DrawTextEx(GetFontDefault(), dialog.Title, title, 10.f, 5.f, Fade(WHITE, alphaTitle));
+            DrawText("TITLE SCREEN ON", 50, 50, 5.f, WHITE);
+        break;
     }
 
 }
 
 void Frontend::mFrontend() {
 
-    float CurrentTime = GetTime();
+
+
+//    float CurrentTime = GetTime();
 
     // if(!Mode3D && !Mode2D)
     // {
     //     Mode2D = true;
     // }
 
-    if(OpeningScene) // && CurrentTime < 90.0f) 
-    {
-    scene.OpeningBegin();
-    textUtils.Draw();
-    scene.SceneDebug();
-        if(CurrentTime > 90.0f)
+//    if(!MenuMode)
+//    {
+        if(OpeningScene) // && CurrentTime < 90.0f) 
         {
-            OpeningScene = false;
-            Mode3D = false;
-            MenuMode = true;
+//        OpeningScene = true;
+        Time += 0.01f;
+        scene.OpeningBegin();
+        textUtils.Draw();
+        scene.SceneDebug();
+            if(Time > 60.0f) // 60 segundos.
+            {
+                StartGame = true;
+            }
         }
+//    }
+
+    if(StartGame)
+    {
+        Mode2D = true;
+        Mode3D = false;
+        OpeningScene = false;
+        MenuMode = false;
     }
 
     if(Mode2D)
     {
 //        Mode2D = false;
 
-        if (MenuMode)
+        switch(MenuMode)
         {
+            case true:
             MainFrontend();
-        }
-        if (!MenuMode)
-        {
+            break;    
+            case false:   
             game.GameBegin();
+            // game.TilemapManager();
+            break;
         }
 
     }
 
         // scene.~Scene();
         // textUtils.~Text3D();
+
+}
+
+void Frontend::Settings() {
+
+    if(settings.VideoSettings)
+    {
+
+
+    }
+
+    if(settings.AudioSettings)
+    {
+
+
+    }
+
+    if(settings.InputSettings)
+    {
+
+
+    }
 
 }
--- StarWarsFangame/frontend.hpp
+++ StarWarsFangame/frontend.hpp
@@ -11,32 +11,65 @@ class Frontend {
         ~Frontend();
         void mFrontend();
         void MainFrontend();
+        void Settings();
 
     private:
         Scene scene;
         Text3D textUtils;
         Game game;
         float CurrentTime;
+        float Time;
         // bool OpeningScene;
         // bool Mode3D;
         // bool Mode2D;
         // bool MenuMode;
         // bool ModeSet;
         Texture2D background;
+        Texture2D logo;
+        Texture2D icon;
         Vector2 title;
         float offset;
         float screenWidth;
         float screenHeight;
         bool MainMenu;
         bool HasPressedTitle;
-        float alpha;
-        char Title2[128] = "Press any button.";
+        bool IsSettings;
+        float alphaTitle;
+        float alphaMenu;
+        bool textBoxEditMode = false;
+        Rectangle Gui;
+        bool StartGame;
+        char TitleEng[128] = "    Press any button.";
 
 
         typedef struct {
         char Title[128] = "Pressione qualquer botão.";
         } Dialog;
         Dialog dialog;
+
+        typedef struct {
+            char Config0[32] = "Vídeo";
+            char Config1[32] = "Áudio";
+            char Config2[32] = "Controles";
+            char Config3[32] = "Acessibilidade";
+        } Config;
+        Config type;
+
+        typedef struct {
+            char Config0[32] = "Video";
+            char Config1[32] = "Audio";
+            char Config2[32] = "Controls";
+            char Config3[32] = "Accessibility";
+        } ConfigEng;
+        ConfigEng typeEng;
+
+        typedef struct {
+            bool VideoSettings;
+            bool AudioSettings;
+            bool InputSettings;
+            bool AccessSettings;
+        } SettingsWindow;
+        SettingsWindow settings;
 
         bool OpeningScene;
         bool Mode2D;

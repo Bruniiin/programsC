6ec5982887ab231d2f4e208e59ea3be814327909
João Lucas B. Lauritzen <lukemagike@gmail.com>
2024-08-08 23:38:40 -0300

    forgot to commit

--- StarWarsFangame/laser.cpp
+++ StarWarsFangame/laser.cpp
@@ -7,20 +7,22 @@
 Laser::Laser()
 {
 
+    pLaser = LoadTexture("graphics/aim_p1.png");
+
 }
 
-Laser::Laser(Vector2 pPosition, Vector2 pDest, Vector2 pDest2, int pSpeed, Color pColor)
+Laser::Laser(Vector2 pPosition, Vector2 pDest, Vector2 pDest2, Rectangle rec, int pSpeed, Color pColor)
 {
 
-    this->pPosition = pPosition;
+    // this->Position = pPosition;
     // this->pPos = pPosition;
-    this->pSpeed = pSpeed;
+    // this->pSpeed = pSpeed;
     this->pAiming = pAiming;
-    this->pColor = pColor;
-    this->pDest = pDest;
-    this->pDest2 = pDest2;
+    // this->pColor = pColor;
+    // this->pDest = pDest;
+    this->pDest = pDest2;
 
-    pLaser = LoadTexture("graphics/aim_p1.png");
+    positions.push_back({pPosition.x, pPosition.y});
 
 
 	// angle = atan2(pPosition.y, pPosition.x) * 180 / PI;
@@ -37,29 +39,61 @@ Laser::~Laser() {
 
 }
 
-void Laser::Init() {
+void Laser::Init(int pType, Vector2 pos, Vector2 dest, Vector2 dest2, int speed2, Color color2) {
 
-
+    // switch(pType)
+    // {
+    //     case 0:
+    //         lasers.push_back(Laser(pos, dest, dest2, (Rectangle){pos.x, pos.y, 50, 50}, 7, BLUE));
+    //         // lasers2.push_back({pos, dest, speed2, color2}); 
+    //         // positions.push_back({pPosition.x, pPosition.y});
+    //     default:
+    //     break;
+    // }
 
 
 
 }
 
-void Laser::Draw() {
+void Laser::Render() {
+
+    for (auto& lasers : lasers)
+    {
+        DrawTextureV(pLaser, lasers.pPosition, lasers.pColor);
+    }
 
     // DrawRectangle
     // (pPosition.x, pPosition.y, 4, 15, BLUE);
 
-    DrawTextureV
-    (pLaser, pPosition, pColor);
+    //for (int i = 0; i < positions.size(); i++)
+    //{
+
+    // for (auto& position : positions)
+    // {
+    //     DrawTextureV
+    //     (pLaser, position, pColor);
+    // }
 
 }
 
-void Laser::SetPos(Vector2 pos) {
+// void Laser::SetPos(Vector2 pos) {
 
-    //this->pPosition = pPosition;
+     //this->pPosition = pPosition;
 
-}
+// }
+
+// void Laser::Test2()
+// {
+
+//     for(auto& laser: lasers) {
+//         laser.Draw();
+//     }
+
+//     for(auto& laser: lasers) {
+//         laser.Update();
+//     }
+
+// }
 
 Vector2 Laser::Dir() {
 
@@ -73,26 +107,48 @@ Vector2 Laser::Dir() {
 
 void Laser::Update() {
 
-        Vector2 changeVector = Vector2Scale(pDest, GetFrameTime());
-        pPos = Vector2Add(pPos, changeVector);
-        currentRange += Vector2Length(changeVector);
-        
-        pPosition.x += pDest2.x * pSpeed;
-        pPosition.y -= pDest2.y * pSpeed;
+    float dt = GetFrameTime();
 
+    for(auto& lasers : lasers)
+    {
+        lasers.pPosition.x += lasers.pDest.x * lasers.pSpeed;
+        lasers.pPosition.y -= lasers.pDest.y * lasers.pSpeed;
+
+        lasers.pCurrentRange += 1.0f * dt;
+
+        if(lasers.pCurrentRange >= lasers.pMaxRange)
+        {
+            lasers.isAlive = false;
+        }
+    }
+
+    for(auto it = lasers.begin(); it != lasers.end(); )
+    {
+        if (!it->isAlive) {
+        it = lasers.erase(it);
+        }
+        else {
+            ++it;
+        }
+    }
+
+    // Vector2 changeVector = Vector2Scale(pDest, GetFrameTime());
+    // pPos = Vector2Add(pPos, changeVector);
+    // currentRange += Vector2Length(changeVector);
+    // for(auto& position : positions)
+    // {
+    //     position.x += pDest.x * pSpeed;
+    //     position.y -= pDest.y * pSpeed;
+    // }
 	// pPosition.y *= -1;
 	// angle = atan2(pPosition.y, pPosition.x) * 180 / PI;
 	// if (pPosition.y <= 0) angle += 360;
-
     // Vector2 pPosition = Dir();
-
     // pPosition.y *= -1;
     // angle = atan2(pPosition.y, pPosition.x) * 180 / PI;
     // if (pPosition.y <= 0) angle += 360;
-    
 	// float angleSpread;
 	// angleSpread = angle;
-
 	// Vector2 direction = { cos(angleSpread * PI / 180), -sin(angleSpread * PI / 180) };
 	// Vector2 pSpeed = Vector2Scale(pPosition, 7.f);
 
--- StarWarsFangame/laser.hpp
+++ StarWarsFangame/laser.hpp
@@ -7,28 +7,43 @@ class Laser {
 
     public:
         Laser();
-        Laser(Vector2 pPosition, Vector2 pDest, Vector2 pDest2, int pSpeed, Color color);
+        Laser(Vector2 pPosition, Vector2 pDest, Vector2 pDest2, Rectangle pRectangle, int pSpeed, Color color);
         ~Laser();
-        void Draw();
+        void Render();
         void Update();
-        void Init();
+        void Init(int pType, Vector2 pos, Vector2 dest, Vector2 dest2, int speed2, Color color2);
         void Projectile(Vector2 pPosition, int pSpeed, Color color);
         void SetPos(Vector2 pos);
+        void Test2();
         Vector2 Dir();
         Vector2 pPos;
         float currentRange;
+
+        typedef struct {
+            Vector2 pPosition;
+            Vector2 pDest;
+            int pSpeed;
+            int pCurrentRange;
+            int pMaxRange;
+            bool isAlive;
+            Color pColor;
+        } sLaser;
 
         float angle;
 
         std::vector<float> angles;
 
-        std::vector<Laser> lasers;
+        // std::vector<Laser> lasers;
+        std::vector<sLaser> lasers;
 
+        std::vector<Vector2>positions;
 
-    private:
-        Vector2 pPosition;
+        Vector2 Position;
         Vector2 pDest;
         Vector2 pDest2;
+
+    private:
+
         int pSpeed;
         Texture2D pLaser;
         bool pAiming;
